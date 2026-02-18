c42e3164c8b00b67dca83b98d42af057b51807b0
Hamsa1913 <Hamsa12021@gmail.com>
2026-02-18 11:57:19 +0300

    Refactor __init__.py to include model imports

--- part2/app/models/__init__.py
+++ part2/app/models/__init__.py
@@ -1,1 +1,9 @@
-#part2/app/models/__init__.py
+# part2/app/models/__init__.py
+
+from .BaseModel import BaseModel
+from .user import User
+from .place import Place
+from .review import Review
+from .amenity import Amenity
+
+__all__ = ["BaseModel", "User", "Place", "Review", "Amenity"]
