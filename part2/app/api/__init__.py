ea06bf33e70a17b775ffc2b9a663baed9e69810a
Rabea Thabit <rabea14180@gmail.com>
2026-02-19 23:42:30 +0300

    Add places namespace to API initialization

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.places import api as places_ns  # assuming places.py exists
 
 def create_api(app):
     api = Api(app, version='1.0', title='HBnB API',
@@ -10,4 +11,5 @@ def create_api(app):
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(places_ns, path='/api/v1/places')
     return api
