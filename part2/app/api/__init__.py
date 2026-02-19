e9bc919ebbfbb4d4b0f50869bcdc6a7fbf5942d8
Rabea Thabit <rabea14180@gmail.com>
2026-02-19 17:58:40 +0300

    Add amenities namespace to API initialization

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -1,29 +1,12 @@
 #part2/app/api/__init__.py
-#!/usr/bin/python3
-"""
-API initialization module
-
-Creates the main API object and registers namespaces.
-"""
-
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
-
+from app.api.v1.amenities import api as amenities_ns
 
 def create_api(app):
-    """
-    Create and configure the API
-    """
-
-    api = Api(
-        app,
-        version="1.0",
-        title="HBnB API",
-        description="HBnB Evolution REST API",
-        doc="/api/v1/docs"  # Swagger documentation endpoint
-    )
-
-    # Register namespaces
-    api.add_namespace(users_ns, path="/api/v1/users")
-
+    api = Api(app, version='1.0', title='HBnB API',
+              description='HBnB Evolution REST API',
+              doc='/api/v1/docs')
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
     return api
