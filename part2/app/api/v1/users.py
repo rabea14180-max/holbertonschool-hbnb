496e57a368be3b6f061b75c3c00bb9a09fe37b1c
Rabea Thabit <rabea14180@gmail.com>
2026-02-19 16:07:51 +0300

    Refactor user operations to use HBnBFacade

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -1,10 +1,11 @@
 #part2/app/api/v1/users.py
 from flask_restx import Namespace, Resource, fields
-from app.business.user_service import UserService
+from app.services.facade import HBnBFacade
 
 api = Namespace('users', description='User operations')
+facade = HBnBFacade()
 
-# Model used for input (includes password)
+# Input model (includes password)
 user_input_model = api.model('UserInput', {
     'first_name': fields.String(required=True),
     'last_name': fields.String(required=True),
@@ -13,7 +14,7 @@ user_input_model = api.model('UserInput', {
     'is_admin': fields.Boolean(required=False)
 })
 
-# Model used for output (NO password)
+# Output model (NO password)
 user_output_model = api.model('UserOutput', {
     'id': fields.String,
     'first_name': fields.String,
@@ -33,15 +34,15 @@ class UserList(Resource):
     def post(self):
         """Create a new user"""
         data = api.payload
-        user = UserService.create_user(data)
-        return user, 201
+        user = facade.create_user(data)
+        return user.to_dict(), 201
 
 
     @api.marshal_list_with(user_output_model)
     def get(self):
         """Retrieve list of users"""
-        users = UserService.get_all_users()
-        return users, 200
+        users = facade.get_all_users()
+        return [u.to_dict() for u in users], 200
 
 
 @api.route('/<string:user_id>')
@@ -50,12 +51,10 @@ class UserResource(Resource):
     @api.marshal_with(user_output_model)
     def get(self, user_id):
         """Retrieve user by ID"""
-        user = UserService.get_user_by_id(user_id)
-
+        user = facade.get_user(user_id)
         if not user:
             api.abort(404, "User not found")
-
-        return user, 200
+        return user.to_dict(), 200
 
 
     @api.expect(user_input_model, validate=False)
@@ -63,9 +62,7 @@ class UserResource(Resource):
     def put(self, user_id):
         """Update user information"""
         data = api.payload
-        user = UserService.update_user(user_id, data)
-
+        user = facade.update_user(user_id, data)
         if not user:
             api.abort(404, "User not found")
-
-        return user, 200
+        return user.to_dict(), 200
