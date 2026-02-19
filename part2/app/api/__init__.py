4b1e70d32b3c98be808a2391009ea57ee7a2533a
Rabea Thabit <rabea14180@gmail.com>
2026-02-19 22:46:04 +0300

    Add reviews namespace to API initialization

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -2,11 +2,12 @@
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.reviews import api as reviews_ns
 
 def create_api(app):
     api = Api(app, version='1.0', title='HBnB API',
-              description='HBnB Evolution REST API',
-              doc='/api/v1/docs')
+              description='HBnB Evolution REST API', doc='/api/v1/docs')
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
     return api
