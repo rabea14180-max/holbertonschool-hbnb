732455cbd08a3cf09eb9de04a247ceedd6749bf9
Solaf Alessa <solaff.alessa@gmail.com>
2026-02-20 17:37:44 +0300

    Remove unnecessary facade instantiation

--- part2/app/api/v1/reviews.py
+++ part2/app/api/v1/reviews.py
@@ -3,7 +3,6 @@ from flask_restx import Namespace, Resource, fields
 from app.services import facade
 
 api = Namespace('reviews', description='Review operations')
-facade = HBnBFacade()
 
 review_model = api.model('Review', {
     'text': fields.String(required=True),
