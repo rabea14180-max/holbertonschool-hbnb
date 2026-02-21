0f449dffc0289f3220b52e713591c99a6e18a32d
Solaf Alessa <solaff.alessa@gmail.com>
2026-02-21 22:05:19 +0300

    Update facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -4,7 +4,6 @@ from app.models.amenity import Amenity
 from app.models.user import User
 from app.models.place import Place
 from app.models.review import Review
-from app.services import facade
 
 class HBnBFacade:
     def __init__(self):
