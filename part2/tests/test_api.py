88de215c3a08cd430fb48b7fe22687037cc9f998
Solaf Alessa <solaff.alessa@gmail.com>
2026-03-10 02:40:27 +0300

    Update test_api.py

--- part2/tests/test_api.py
+++ part2/tests/test_api.py
@@ -1,5 +1,5 @@
 import pytest
-from app import app
+from run import app
 
 
 @pytest.fixture
@@ -11,26 +11,6 @@ def client():
 def test_get_users(client):
     response = client.get("/api/v1/users")
     assert response.status_code == 200
-
-
-def test_create_user(client):
-    data = {
-        "name": "Ali",
-        "email": "ali@test.com"
-    }
-
-    response = client.post("/api/v1/users", json=data)
-    assert response.status_code in [200, 201]
-
-
-def test_create_invalid_user(client):
-    data = {
-        "name": "",
-        "email": "invalid"
-    }
-
-    response = client.post("/api/v1/users", json=data)
-    assert response.status_code == 400
 
 
 def test_get_places(client):
