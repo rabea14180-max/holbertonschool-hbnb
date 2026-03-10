c25a501a28299a5045e991a7e9e9929b8a88e1b1
Hamsa1913 <Hamsa12021@gmail.com>
2026-03-10 06:25:11 +0300

    Update __init__.py

--- part3/app/api/v1/__init__.py
+++ part3/app/api/v1/__init__.py
@@ -1,2 +1,13 @@
-#part3/app/api/v1/__init__.py
-#!/usr/bin/python3
+from flask_restx import Api
+from .users import api as users_ns
+from .amenities import api as amenities_ns
+
+api = Api(
+    title="HBnB API",
+    version="1.0",
+    description="HBnB Application API"
+)
+
+# Add namespaces so they appear in Swagger
+api.add_namespace(users_ns)
+api.add_namespace(amenities_ns)
