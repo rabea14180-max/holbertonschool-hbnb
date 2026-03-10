d99978594a9e135f3a4d09e1db8b0ae990d95ad6
Solaf Alessa <solaff.alessa@gmail.com>
2026-03-10 03:48:52 +0300

    Update user.py

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -37,9 +37,9 @@ class User(BaseModel):
 
         if not isinstance(self.is_admin, bool):
             raise ValueError("is_admin must be a boolean")
-
+            
         if not isinstance(self.password, str) or not self.password.strip():
-        raise ValueError("password is required")
+            raise ValueError("password is required")
 
     def add_place(self, place):
         if place not in self.places:
