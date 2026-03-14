6b8b2cb51eab810b0cc5eea9b289453d1f0bbbde
Solaf Alessa <solaff.alessa@gmail.com>
2026-03-14 20:19:47 +0300

    Update __init__.py

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import amenity_bp
+from app.api.v1.amenities import amenity_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.places import api as places_ns  # assuming places.py exists
 
@@ -16,5 +16,5 @@ def create_api():
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(places_ns, path='/api/v1/places')
-    app.register_blueprint(amenity_bp, url_prefix='/api/v1')
+   
     return app
