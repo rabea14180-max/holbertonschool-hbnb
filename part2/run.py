f2910f427f6b2b47f35a33f7aba8a315f03fd23a
Solaf Alessa <solaff.alessa@gmail.com>
2026-03-14 20:54:29 +0300

    Update run.py

--- part2/run.py
+++ part2/run.py
@@ -1,7 +1,16 @@
 # part2/run.py
-from app.api import create_api
+def create_app():
+    from flask import Flask
+    from flask_restx import Api
+    from app.api.user import UserList
+    from app.api.amenity import AmenityList
+    from app.api.place import PlaceList
 
-app = create_api()
+    app = Flask(__name__)
+    api = Api(app, prefix="/api/v1")
+    
+    api.add_resource(UserList, '/users/')
+    api.add_resource(AmenityList, '/amenities/')
+    api.add_resource(PlaceList, '/places/')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    return app
