bb9fe39734c7fe651480e2cfcdb59ef1e10d6ad3
Solaf Alessa <solaff.alessa@gmail.com>
2026-03-14 20:21:37 +0300

    Update __init__.py

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import amenity_ns
+from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.places import api as places_ns  # assuming places.py exists
 
